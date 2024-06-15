be34199184baec7b319eb2cf1b780dd9762d45f3
Cosmin-Mare <cosmintudor.mare@cnamd.onmicrosoft.com>
2024-06-14 22:42:45 -0400

    add more qualities

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,14 @@ export default function Home() {
           <p class="item-title3">e goofy</p>
           <img src="/3.jpg" class="item-img3"/>
         </div>
+        <div class="item3">
+          <p class="item-title3">e foarte harnica (cand vrea)</p>
+          <img src="/4.webp" class="item-img3"/>
+        </div>
+        <div class="item3">
+          <p class="item-title3">ma face sa ma simt iubit</p>
+          <img src="/5.jpg" class="item-img3"/>
+        </div>
       </div>
       <div class="te-iubesc">
         <p>ps: daca esti rebe: te iubesc❤️</p>
